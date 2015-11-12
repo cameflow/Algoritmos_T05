863efaacdc70d9433d4eff71f8392f084f40a650
Alejandro <alex.terrazas.z@gmail.com>
2015-11-12 08:08:08 -0600

    :recycle: Clean code and add comments in mainwindow.h and mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,58 +11,64 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
 
+//Función que abre la ventana de polígono
 void MainWindow::on_pushButton_clicked()
 {
-    printf("botón1\n");
-    poligono poli;
-    poli.setModal(true);
-    poli.exec();
+  printf("botón1\n");
+  poligono poli;
+  poli.setModal(true);
+  poli.exec();
 }
 
+//Función que abre la ventana de arco
 void MainWindow::on_pushButton_2_clicked()
 {
-    printf("botón2\n");
-    arco arc;
-    arc.setModal(true);
-    arc.exec();
+  printf("botón2\n");
+  arco arc;
+  arc.setModal(true);
+  arc.exec();
 }
 
+//Función que abre la ventana de cubo
 void MainWindow::on_pushButton_3_clicked()
 {
-    printf("botón3\n");
-    cubo cu;
-    cu.setModal(true);
-    cu.exec();
+  printf("botón3\n");
+  cubo cu;
+  cu.setModal(true);
+  cu.exec();
 }
 
+//Función que abre la ventana de prisma triangular
 void MainWindow::on_pushButton_4_clicked()
 {
-    printf("botón4\n");
-    prismaTriangular prismaT;
-    prismaT.setModal(true);
-    prismaT.exec();
+  printf("botón4\n");
+  prismaTriangular prismaT;
+  prismaT.setModal(true);
+  prismaT.exec();
 }
 
+//Función que abre la ventana de PrismaRectangular
 void MainWindow::on_pushButton_5_clicked()
 {
-    printf("botón5\n");
-    PrismaRectangular prismaR;
-    prismaR.setModal(true);
-    prismaR.exec();
+  printf("botón5\n");
+  PrismaRectangular prismaR;
+  prismaR.setModal(true);
+  prismaR.exec();
 }
 
+//Función que abre la ventana de Cono
 void MainWindow::on_pushButton_6_clicked()
 {
-    printf("botón6\n");
-    cono co;
-    co.setModal(true);
-    co.exec();
+  printf("botón6\n");
+  cono co;
+  co.setModal(true);
+  co.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,17 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+  //Botonoes de la pantalla principal
 private slots:
     void on_pushButton_clicked();
-
     void on_pushButton_2_clicked();
-
     void on_pushButton_3_clicked();
-
     void on_pushButton_4_clicked();
-
     void on_pushButton_5_clicked();
-
     void on_pushButton_6_clicked();
 
 private:
